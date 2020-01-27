401ee338a1b299ccf0081a55b53a579e4cf09892
K Raja Ravindra <rajaravindra919@gmail.com>
2020-01-27 13:08:20 +0530

    Modifications

--- master.sh
+++ master.sh
@@ -1,10 +1,101 @@
-sudo apt-get update
-sudo apt-get install wget -y
-wget https://apt.puppetlabs.com/puppet-release-bionic.deb
-sudo dpkg -i puppet-release-bionic.deb
-sudo apt-get install puppetmaster -y
-#apt policy puppetmaster
-#sudo systemctl status puppet-master.service
-sudo systemctl restart puppet-master.service
-sudo ufw allow 8140/tcp
-sudo echo "$0 puppet" >> /etc/hosts
+#!/bin/bash
+packages="wget figlet boxes pv"
+Initialiser()
+{
+    clear
+        sudo apt-get update
+    clear
+        echo "Installing requested packages" | pv -qL 10
+    sleep 1s
+        sudo apt-get install $packages -y
+    clear
+        figlet -c -t -k Welcome
+        echo "Downloading and Installing puppermaster" | pv -qL 10
+        wget https://apt.puppetlabs.com/puppet-release-bionic.deb
+    sleep 1s
+        sudo dpkg -i puppet-release-bionic.deb
+        sudo apt-get install puppetmaster -y
+        apt policy puppetmaster
+        sudo rm -rf *.deb.* *.deb
+        sudo systemctl restart puppet-master.service
+    clear
+        echo "Installed puppet master services " | pv -qL 10
+    sleep 1s
+        echo "Allowing port 8140 TCP" | pv -qL 10
+    sleep 1s
+        sudo ufw allow 8140/tcp
+    echo "
+    
+    "
+}
+Initialiser
+
+writeHosts(){
+    figlet -t -c -k "Add  slave  machines".
+    read -p "Number of slave machines: " var
+    i=1
+    while [ $i -le $var ]
+    do
+        read -p "Enter IP address of $i : " ipvalue
+        sudo echo "$ipvalue puppet" >> /etc/hosts
+        let i+=1
+    done
+    echo "Writing hosts" | pv -qL 10
+    sleep 1s
+}
+writeHosts
+
+CertificationSign() {
+    sudo systemctl restart puppet-master.service puppet.service
+    sudo puppet cert sign --all
+}
+CertificationSign
+
+manifestoFile(){
+    sudo mkdir -p /etc/puppet/code/environments/production/manifests
+    cd /etc/puppet/code/environments/production/manifests
+    read -p "Do you want to write proceed with sample manifesto? [Y/N]: " yn
+    case $yn in   
+    [Nn]*)  
+            sudo rm -rf *.pp;
+            sudo nano site.pp;;
+    [Yy]*)
+            sudo echo "#sample nginx server
+            node default{ 
+            package{'nginx':
+            ensure => installed,
+            }
+            file{'/tmp/status.txt':
+            content=> \"nginx installed\",
+            mode=> '0644',
+            }
+            }" > site.pp;
+            echo "
+            
+            "
+            cat site.pp | pv -qL 25;;
+    *) echo "Please answer yes or no.";;
+    esac
+    echo "
+    Proceed to slave and execute \"sudo puppet agent --test\"" | pv -qL 10
+    sleep 2s
+}
+
+read -p "Proceed creating manifesto? [Y/N]: " req
+case $req in
+[Yy]*)  
+      manifestoFile;;
+[Nn]*)
+      ;;      
+*) echo "Please answer yes or no.";;
+esac
+
+echo "
+
+"
+figlet -c -t -k END
+sleep 1s
+echo "
+
+"
+echo "Support by starring this repo at https://github.com/KR-Ravindra/PuppetScripts.git" | boxes
--- slave.sh
+++ slave.sh
@@ -1,10 +1,52 @@
-sudo apt-get update
-sudo apt-get install wget
-wget https://apt.puppetlabs.com/puppet-release-bionic.deb
-sudo dpkg -i puppet-release-bionic.deb
-sudo apt-get install puppet
-apt policy puppet
-#sudo systemctl status puppet.service
-sudo systemctl restart puppet.service
-sudo ufw allow 8140/tcp
-sudo echo "$0 puppet" >> /etc/hosts
+#!/bin/bash
+packages="wget figlet boxes pv"
+Initialiser()
+{
+    clear
+        sudo apt-get update
+    clear
+        echo "Installing requested packages" | pv -qL 10
+    sleep 1s
+        sudo apt-get install $packages -y
+    clear
+        figlet -c -t -k Welcome
+        echo "Downloading and Installing puppet" | pv -qL 10
+        wget https://apt.puppetlabs.com/puppet-release-bionic.deb
+    sleep 1s
+        sudo dpkg -i puppet-release-bionic.deb
+        sudo apt-get install puppet -y
+        apt policy puppet
+        sudo rm -rf *.deb.* *.deb
+        sudo systemctl restart puppet.service
+    clear
+        echo "Installed puppet services " | pv -qL 10
+    sleep 1s
+        echo "Allowing port 8140 TCP" | pv -qL 10
+    sleep 1s
+        sudo ufw allow 8140/tcp
+    echo "
+    
+    "
+}
+Initialiser
+
+writeHosts(){
+    figlet -t -c -k "Add  master  machine".
+    
+        read -p "Enter IP address of master : " ipvalue
+        sudo echo "$ipvalue puppet" >> /etc/hosts
+ 
+    echo "Writing hosts" | pv -qL 10
+    sleep 1s
+}
+writeHosts
+echo "
+
+"
+figlet -c -t -k END
+sleep 1s
+echo "
+
+"
+echo "Support by starring this repo at https://github.com/KR-Ravindra/PuppetScripts.git" | boxes
+
